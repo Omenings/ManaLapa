a63cdfcf3524020ddc06306811cd52edd6aaf6a9
Omenings <aimens.veveris12@gmail.com>
2025-06-10 21:37:18 -0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Novērš pārlādēšanu
+  alert("Paldies par ziņu! Es ar tevi sazināšos.");
+});
