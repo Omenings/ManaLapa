f647fc1226aaad93acf8d026621a1fc428078753
Omenings <aimens.veveris12@gmail.com>
2025-06-13 08:33:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-  event.preventDefault(); // Novērš pārlādēšanu
-  alert("Paldies par ziņu! Es ar tevi sazināšos.");
-});
 <script>
   const toggleButton = document.getElementById('theme-toggle');
 
